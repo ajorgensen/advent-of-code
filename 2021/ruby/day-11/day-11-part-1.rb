61336e416ec261a2a980527156a0b2403fd4c9de
Andrew Jorgensen <andrew@andrewjorgensen.com>
2021-12-15 14:46:47 -0500

    Implement day 11
    Most of the time was spent figuring out how to pretty print the grid

---
+++ 2021/ruby/day-11/day-11-part-1.rb
@@ -0,0 +1,135 @@
+#!/usr/bin/env ruby
+
+file_path = File.expand_path("../day-11-input.txt", __FILE__)
+input = File.read(file_path)
+
+def clear_terminal
+  puts "\e[H\e[2J"
+end
+
+def colorize(str, color_code)
+  "\e[#{color_code}m#{str}\e[0m"
+end
+
+class Grid
+  attr_reader :grid
+
+  def initialize(grid)
+    @grid = grid
+  end
+
+  def pretty_print
+    grid.each do |row|
+      row.each do |col|
+
+        if col == 0
+          print "#{colorize(col, 34)} "
+        else
+          print "#{col} "
+        end
+      end
+      puts ""
+    end
+  end
+
+  def each(&block)
+    grid.each_with_index do |row, x|
+      row.each_with_index do |elem, y|
+        yield elem, x, y
+      end
+    end
+  end
+
+  def set(x,y,val)
+    grid[x][y] = val
+  end
+
+  def get(x,y)
+    grid[x][y]
+  end
+
+  def adjacent(x, y)
+    adjacent = []
+
+    if x - 1 >= 0 && y - 1 >= 0
+      adjacent << [x - 1, y - 1]
+    end
+
+    if x - 1 >= 0
+      adjacent << [x - 1, y]
+    end
+
+    if x - 1 >= 0 && y + 1 < grid.first.length
+      adjacent << [x - 1, y + 1]
+    end
+
+    if y - 1 >= 0
+      adjacent << [x, y - 1]
+    end
+
+    if y + 1 < grid.first.length
+      adjacent << [x, y + 1]
+    end
+
+    if x + 1 < grid.length && y - 1 >= 0
+      adjacent << [x + 1, y - 1]
+    end
+
+    if x + 1 < grid.length
+      adjacent << [x + 1, y]
+    end
+
+    if x + 1 < grid.length && y + 1 < grid.first.length
+      adjacent << [x + 1, y + 1]
+    end
+
+    adjacent
+  end
+end
+
+def do_step(grid)
+  flashes = 0
+  grid.each { |elem,x,y| grid.set(x,y, elem + 1) }
+  to_flash = []
+  grid.each { |elem,x,y| to_flash << [x,y] if elem == 10 }
+
+  while !to_flash.empty?
+    flashes += 1
+    x,y = to_flash.pop
+
+    adjacent = grid.adjacent(x,y)
+    adjacent.each do |x,y|
+      elem = grid.get(x,y)
+      
+      if elem < 9
+        grid.set(x,y,elem + 1)
+      elsif elem == 9
+        to_flash << [x,y]
+        grid.set(x,y,elem + 1)
+      end
+    end
+  end
+
+  grid.each { |elem,x,y| grid.set(x,y,0) if elem >= 10 }
+  flashes
+end
+
+grid = input.split("\n").map { |l| l.split("").map(&:to_i) }
+grid = Grid.new(grid)
+
+clear_terminal
+puts "Before any steps"
+grid.pretty_print
+
+flashes = 0
+steps = 100
+steps.times do |i|
+  clear_terminal
+
+  flashes += do_step(grid)
+
+  puts "After Step #{i + 1}"
+  grid.pretty_print
+end
+
+puts "Number of flashes after #{steps}: #{flashes}"
